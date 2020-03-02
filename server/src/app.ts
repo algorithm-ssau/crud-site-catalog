931ec618dcf8e0865aa633e16124b3178a104721
Max <symraklabs@gmail.com>
2020-03-02 12:53:50 +0300

    update package.json and add controllers

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,20 +1,19 @@
 import express from "express";
-import path from "path";
 import mongoose from "mongoose";
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express();
 const port = 8080;
 
+app.use(bodyParser.json());
+app.use(cors());
+
+// connect to db
 mongoose.connect("mongodb://localhost:27017/crud-site-catalog", { useNewUrlParser: true })
   .then(() => console.log("connected"))
-  .catch(err => {
-      return console.log(err);
-  });
-
-app.get( "/", ( req, res ) => {
-    res.send( "hello" );
-} );
+  .catch(err => console.log(err));
 
 app.listen( port, () => {
     console.log( `server started at http://localhost:${ port }` );
-} );
+});

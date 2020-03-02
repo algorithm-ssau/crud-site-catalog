5d3272cdcfbebc3621008b19a4b6b6c58308b2c3
PolushkinDenis <tigras666@mail.ru>
2020-03-02 10:40:16 +0300

    add mongoose and schemes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,15 @@
 import express from "express";
 import path from "path";
+import mongoose from "mongoose";
 
 const app = express();
 const port = 8080;
+
+mongoose.connect("mongodb://localhost:27017/crud-site-catalog", { useNewUrlParser: true })
+  .then(() => console.log("connected"))
+  .catch(err => {
+      return console.log(err);
+  });
 
 app.get( "/", ( req, res ) => {
     res.send( "hello" );

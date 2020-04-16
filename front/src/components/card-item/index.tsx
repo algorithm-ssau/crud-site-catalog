558e39aa5601a66916c319929681dc409844bee3
mkrivoshchapov <maks.krivoshapov@yandex.ru>
2020-04-16 22:32:36 +0400

    next fix

--- front/src/components/card-item/index.tsx
+++ front/src/components/card-item/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Modal } from '@material-ui/core';
+import { Modal, Button } from '@material-ui/core';
 // eslint-disable-next-line no-unused-vars
 import * as H from 'history';
 import './styles.scss';
@@ -9,15 +9,25 @@ interface CardItemPrpos {
     description: string;
     imageUrl: string;
     isOpen: boolean;
+    onClose: (event: any) => void;
 }
 
-const CardItems: React.FC<CardItemPrpos> = ({price, description}) => {
+const CardItem: React.FC<CardItemPrpos> = ({price, description, imageUrl, isOpen, onClose}) => {
   console.log(history);
   return (
-    <Modal> 
-        <Image/>
+    <Modal open={isOpen} onClose={onClose}>
+        <div className="card-item">
+            <div className="card-item__photo-container">
+                <h2 className="card-item__photo-text">Фото</h2>
+                <img className="card-item__photo" src={imageUrl} />
+            </div>
+            <div className="card-item__button-conainer">
+                <Button className="card-item__button">Описание</Button>
+                <Button className="card-item__button">Удалить</Button>
+            </div>
+        </div>
     </Modal>
   );
 };
 
-export default MainPage;
+export default CardItem;

356b3d955304fd0b35c01aa5eb007f6a5d874e2b
Max <symraklabs@gmail.com>
2020-05-17 20:18:49 +0400

    add modal dialog to card

--- front/src/components/componentWepPage/images.tsx
+++ front/src/components/componentWepPage/images.tsx
@@ -4,6 +4,7 @@ import Grid, { GridSpacing } from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import React, { useState, useEffect } from 'react';
 import MediaCard from './card';
+import { CardItem } from '..';
 
 const useStyles = makeStyles((theme: Theme) => createStyles({
   root: {
@@ -30,25 +31,49 @@ export default function SpacingGrid({ products }: { products: GridItem[] }) {
   const classes = useStyles();
   const [open, setOpen] = React.useState(false);
 
+  const [modalProps, setModalProps] = useState({} as any);
+  const [isModalOpen, setModalOpen] = useState(false);
+  const handleOpenModal = (item: GridItem) => {
+    setModalProps({
+      price: item.price,
+      description: item.description,
+      onClose: () => setModalOpen(false),
+      imageUrl:
+        "https://assets.change.org/photos/9/ti/kt/OZTikTiXHKSEJIl-800x450-noPad.jpg?1549309383",
+    });
+  setModalOpen(true);
+};
 
-  const renderGridItem = (gridItems: GridItem[]) => gridItems.map((gridItem) => (
-    <Grid item>
-      <Paper className={classes.paper}><MediaCard /></Paper>
-    </Grid>
-  ));
+
+  const renderGridItem = (gridItems: GridItem[]) =>
+    gridItems.map((gridItem) => (
+      <Grid item>
+        <Paper className={classes.paper} onClick={() => handleOpenModal(gridItem)}>
+          <MediaCard />
+        </Paper>
+      </Grid>
+    ));
 
 
   return (
+    <>
+      <CardItem
+            isOpen={isModalOpen}
+            price={modalProps.price}
+            description={modalProps.description}
+            onClose={modalProps.onClose}
+            imageUrl={modalProps.imageUrl}
+      />
+      
     <Grid container className={classes.root} spacing={7}>
       <Grid item xs={12}>
         <Grid container spacing={spacing}>
+          
 
           {renderGridItem(products)}
-
         </Grid>
       </Grid>
-    </Grid>
-
-
+      </Grid>
+      </>
   );
 }

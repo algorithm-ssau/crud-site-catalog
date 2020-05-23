d6a2e9825af90507c0ae37f3e278bc988e32daba
PolushkinDenis <tigras666@mail.ru>
2020-05-23 13:45:43 +0400

    add python template

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,9 +1,17 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import render_template
+from flask_pymongo import PyMongo
 
-@app.route('/')
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb+srv://Team06:TTD07KlWDvD59Zrp@test-cluster-ggkus.mongodb.net/test?retryWrites=true&w=majority"
+mongo = PyMongo(app)
+
+@app.route('/about')
 def hello_world():
-    return 'Hello World!'
+    response = []
+    temp = mongo.db.products.find({})
+
+    return render_template("task.html", temp=list(temp))
 
 if __name__ == '__main__':
     app.run()

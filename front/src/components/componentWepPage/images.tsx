5fa9c434cd526725a3cb50640c77635e193f7875
Max <symraklabs@gmail.com>
2020-05-23 17:52:50 +0400

    fix layout

--- front/src/components/componentWepPage/images.tsx
+++ front/src/components/componentWepPage/images.tsx
@@ -1,22 +1,24 @@
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-import Grid, { GridSpacing } from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import React, { useState, useEffect } from 'react';
-import MediaCard from './card';
-import { CardItem } from '..';
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import Grid, { GridSpacing } from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import React, { useState, useEffect } from "react";
+import MediaCard from "./card";
+import { CardItem } from "..";
 
-const useStyles = makeStyles((theme: Theme) => createStyles({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    height: 140,
-    width: 345,
-  },
-  control: {
-    padding: theme.spacing(7),
-  },
-}));
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      height: 140,
+      width: 345,
+    },
+    control: {
+      padding: theme.spacing(7),
+    },
+  })
+);
 export interface GridItem {
   id: string;
   name: string;
@@ -43,16 +45,17 @@ export default function SpacingGrid({ products }: { products: GridItem[] }) {
     setModalOpen(true);
   };
 
-  const renderGridItem = (gridItems: GridItem[]) => gridItems.map((gridItem) => (
-    <Grid item>
-      <Paper
-        className={classes.paper}
-        onClick={() => handleOpenModal(gridItem)}
-      >
-        <MediaCard title={gridItem.name} image={gridItem.imagePath} />
-      </Paper>
-    </Grid>
-  ));
+  const renderGridItem = (gridItems: GridItem[]) =>
+    gridItems.map((gridItem) => (
+      <Grid item>
+        <Paper
+          className={classes.paper}
+          onClick={() => handleOpenModal(gridItem)}
+        >
+          <MediaCard title={gridItem.name} image={gridItem.imagePath} />
+        </Paper>
+      </Grid>
+    ));
 
   return (
     <>
@@ -65,7 +68,12 @@ export default function SpacingGrid({ products }: { products: GridItem[] }) {
         imageUrl={modalProps.imageUrl}
       />
 
-      <Grid container className={classes.root} spacing={7}>
+      <Grid
+        container
+        style={{ width: "100vw" }}
+        className={classes.root}
+        spacing={7}
+      >
         <Grid item xs={12}>
           <Grid container spacing={spacing}>
             {renderGridItem(products)}
